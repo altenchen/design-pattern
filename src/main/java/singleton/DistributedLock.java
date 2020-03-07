c96d6c9a0ef7e6ca1456858afc0a3159f3741739
chenchen <altenchen@aliyun.com>
2020-03-07 21:40:51 +0800

    单例模式的扩展实现（支持参数，线程唯一，集群唯一，多例模式）

---
+++ src/main/java/singleton/DistributedLock.java
@@ -0,0 +1,15 @@
+package singleton;
+
+/**
+ * @description:
+ * @create: 2020/3/7
+ * @author: altenchen
+ */
+public class DistributedLock {
+
+    public void lock(){}
+
+    public void unlock() {
+
+    }
+}
