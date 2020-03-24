f5b363bd20cda2a38d53b2813c7b459d74048444
chenchen <altenchen@aliyun.com>
2020-03-24 22:54:43 +0800

    原型模式V2，满足数据版本始终一致，且不停机更新，但此版本缺点是newKeywords的构建成本比较高（例如从数据库读出10万条数据，然后计算哈希值）

--- src/main/java/designpattern/creation/prototype/PrototypeDemo.java
+++ src/main/java/designpattern/creation/prototype/PrototypeDemo.java
@@ -1,5 +1,7 @@
 package designpattern.creation.prototype;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -10,29 +12,22 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class PrototypeDemo {
 
-    private ConcurrentHashMap<String, SearchWord> currentKeywords = new ConcurrentHashMap<String, SearchWord>();
-    private long lastUpdateTime = -1;
+    private HashMap<String, SearchWord> currentKeywords = new HashMap<String, SearchWord>();
 
     public void refresh() {
-        // 从数据库中取出更新时间 > lastUpdateTime的数据，放入到currentKeywords中
-        List<SearchWord> toBeUpdatedSearchWords = getSearchWords(lastUpdateTime);
-        long maxNewUpdateTime = lastUpdateTime;
+        LinkedHashMap<String, SearchWord> newKeywords = new LinkedHashMap<>();
+
+        //从数据库中取出所有的数据，放入到newKeywords中
+        List<SearchWord> toBeUpdatedSearchWords = getSearchWords();
         for (SearchWord searchWord : toBeUpdatedSearchWords) {
-            if (searchWord.getLastUpdateTime() > maxNewUpdateTime) {
-                maxNewUpdateTime = searchWord.getLastUpdateTime();
-            }
-            if (currentKeywords.containsKey(searchWord.getKeyword())) {
-                currentKeywords.replace(searchWord.getKeyword(), searchWord);
-            } else {
-                currentKeywords.put(searchWord.getKeyword(), searchWord);
-            }
+            newKeywords.put(searchWord.getKeyword(), searchWord);
         }
 
-        lastUpdateTime = maxNewUpdateTime;
+        currentKeywords = newKeywords;
     }
 
-    private List<SearchWord> getSearchWords(long lastUpdateTime) {
-        //TODO:从数据库中取出更新时间 > lastUpdateTime的数据
+    private List<SearchWord> getSearchWords() {
+        //TODO:从数据库中取出所有数据
         return null;
     }
 
