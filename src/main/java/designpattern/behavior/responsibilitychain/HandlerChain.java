2748aa5aac6a238b73a5698aac4d39a30845ffb2
chenchen <chenchen@bitnei.cn>
2020-04-27 20:03:06 +0800

    -[refactor]责任链模式V3，通过列表实现，更加简洁

--- src/main/java/designpattern/behavior/responsibilitychain/HandlerChain.java
+++ src/main/java/designpattern/behavior/responsibilitychain/HandlerChain.java
@@ -1,29 +1,26 @@
 package designpattern.behavior.responsibilitychain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author altenchen
  * @time 2020/4/27
  * @description 功能
  */
 public class HandlerChain {
-    private Handler head = null;
-    private Handler tail = null;
+    private List<IHandler> handlers = new ArrayList<>();
     
-    public void addHandler(Handler handler) {
-        handler.setSuccessor(null);
-        
-        if (head == null) {
-            head = handler;
-            tail = handler;
-            return;
-        }
-        
-        tail.setSuccessor(handler);
+    public void addHandlers(IHandler handler) {
+        this.handlers.add(handler);
     }
     
     public void handle() {
-        if (head != null) {
-            head.handler();
+        for (IHandler handler : handlers) {
+            boolean handled = handler.handle();
+            if (handled) {
+                break;
+            }
         }
     }
     
---
+++ src/main/java/designpattern/behavior/responsibilitychain/IHandler.java
@@ -0,0 +1,10 @@
+package designpattern.behavior.responsibilitychain;
+
+/**
+ * @author altenchen
+ * @time 2020/4/27
+ * @description 功能
+ */
+public interface IHandler {
+    boolean handle();
+}
