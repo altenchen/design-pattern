124eea016a5332ac3030421a0dbb38764929169f
chenchen <chenchen@bitnei.cn>
2020-05-11 13:58:47 +0800

    -[test]基于同一个容器创建的两个不同的迭代器iterator1和iterator2，对iterator1执行一个remove()方法,iterator2接着执行next()方法，由于iterator1已经将容器的modCount改为5，而iterator2在执行next()方法时，会执行会执行checkForComodification()方法，此时expectedModCount还是4，与为5的modCount不相同，触发ConcurrentModificationException异常

--- src/main/java/designpattern/behavior/iterator/ApplicationDemo.java
+++ src/main/java/designpattern/behavior/iterator/ApplicationDemo.java
@@ -1,24 +1,34 @@
 package designpattern.behavior.iterator;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * @author altenchen
  * @time 2020/5/11
  * @description 功能
  */
-public class Demo {
+public class ApplicationDemo {
     public static void main(String[] args) {
         ArrayList<String> names = new ArrayList<>();
         names.add("kobe");
         names.add("paul");
         names.add("jordan");
+        names.add("james");
     
         ArrayIterator<String> iterator = new ArrayIterator<>(names);
         while (iterator.hasNext()) {
             System.out.println("elem=> " + iterator.currentItem());
             iterator.next();
         }
+    
+        Iterator<String> iterator1 = names.iterator();
+        Iterator<String> iterator2 = names.iterator();
+        
+        iterator1.next();
+        iterator1.remove();
+//        iterator1.next();
+        iterator2.next();
     }
 
 }
---
+++ src/main/java/designpattern/behavior/iterator/MockList.java
@@ -0,0 +1,12 @@
+package designpattern.behavior.iterator;
+
+import java.util.List;
+
+/**
+ * @author altenchen
+ * @time 2020/5/11
+ * @description 功能
+ */
+public interface MockList<E> extends List<E> {
+
+}
