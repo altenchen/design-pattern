115425587343ce2ce1718a444a56c28913db5344
chenchen <altenchen@aliyun.com>
2020-05-30 20:28:05 +0800

    -[add]添加ignore文件

--- src/main/java/designpattern/creation/prototype/PrototypeDemo.java
+++ src/main/java/designpattern/creation/prototype/PrototypeDemo.java
@@ -2,10 +2,7 @@ package designpattern.creation.prototype;
 
 import java.io.*;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @description:
